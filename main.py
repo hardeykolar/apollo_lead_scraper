de05765fe4b2083ff3b8ba75a7e16bc230ad0543
hardeykolar <ajadiadekola1@gmail.com>
2024-12-09 11:52:53 +0100

    fixed selector problem and removed some field

--- main.py
+++ main.py
@@ -41,19 +41,24 @@ class WebsiteScraper:
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option("useAutomationExtension", False)
+        prefs = {
+            "credentials_enable_service": False,  # Disable password saving service
+            "profile.password_manager_enabled": False  # Disable password manager
+        }
+        options.add_experimental_option("prefs", prefs)
 
-        options.add_argument("--disable-gpu") 
+        #options.add_argument("--disable-gpu") 
         options.add_argument("--disable-extensions") 
         options.add_argument("--disable-infobars") 
         options.add_argument("--start-maximized") 
         options.add_argument("--disable-notifications")  
 
-        options.add_argument("--headless")
-        options.add_argument('--no-sandbox')
-        options.add_argument("--disable-dev-shm-usage")
+        #options.add_argument("--headless")
+        #options.add_argument('--no-sandbox')
+        #options.add_argument("--disable-dev-shm-usage")
 
         self.driver = webdriver.Chrome(options=options)
-        self.driver.maximize_window()
+        #self.driver.maximize_window()
 
     def login_to_apollo(self):
         self.driver.get(self.url)
@@ -90,78 +95,105 @@ class WebsiteScraper:
 
         full_emails_data = []
         full_phone_numbers = []
+        full_profile_url = []
+        full_company_urls = []
         counter = 0
 
         while True:
             emails_data = []
             phone_numbers = []
-            WebDriverWait(self.driver,20).until(EC.visibility_of_all_elements_located((By.XPATH,'//table//tr//td')))
+            WebDriverWait(self.driver,60).until(EC.visibility_of_all_elements_located((By.XPATH,"//div[contains(@id,'table-row')]")))
 
-            rows = self.driver.find_elements(By.XPATH,'//table//tr')
-            for index,row in enumerate(rows):
-                if index == 0:
-                    continue
-                else:
+            rows = self.driver.find_elements(By.XPATH,"//div[contains(@id,'table-row')]")
+            # //div[contains(@id,'table-row')]
+            #new_rows = self.driver.find_elements(By.XPATH,"//div[contains(@id,'table-row')]")
+            #for new_row in new_rows:
+            #    name = new
 
-                    company_urls.append([item.get_attribute('href') for item in row.find_elements(By.TAG_NAME,'td')[2].find_elements(By.TAG_NAME,'a')])
 
-                    profile_urls.append([item.get_attribute('href') for item in row.find_elements(By.TAG_NAME,'td')[0].find_elements(By.TAG_NAME,'a')])
 
-                    names.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][0])
+            
+            for row in rows:
+                
+                #//div[contains(@id,'table-row')][4]/div[1]//a
+                profile_name = row.find_element(By.XPATH,"./div[1]//a").get_attribute('textContent')
+                names.append(profile_name)
+                profile_url = row.find_element(By.XPATH,"./div[1]//a").get_attribute('href')
+                full_profile_url.append(profile_url)
+                job_title = row.find_element(By.XPATH,"./div[2]/span/span").get_attribute('textContent')
+                titles.append(job_title)
 
-                    titles.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][1])
 
-                    companies.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][2])
+                company_url = row.find_elements(By.XPATH,"./div[3]//a")[1].get_attribute('href')
+                full_company_urls.append(company_url)
+                company_name = row.find_elements(By.XPATH,"./div[3]//a")[1].get_attribute('textContent')
+                companies.append(company_name)
 
-                    contact_locations.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][4])
+                #full_phone_numbers.append(row.find_element(By.XPATH,"./div[5]").get_attribute('textContent'))
+                #company_urls.append([item.get_attribute('href') for item in row.find_elements(By.TAG_NAME,'td')[2].find_elements(By.TAG_NAME,'a')])
 
-                    employees.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][5])
+                #profile_urls.append([item.get_attribute('href') for item in row.find_elements(By.TAG_NAME,'td')[0].find_elements(By.TAG_NAME,'a')])
 
-                    industries.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][7])
+                #names.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][0])
 
-                    keywords.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][8])
+                #titles.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][1])
+
+                #companies.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][2])
+
+                #contact_locations.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][4])
+
+                #employees.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][5])
+
+                #industries.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][7])
+
+                #keywords.append([val.get_attribute('textContent') for val in row.find_elements(By.TAG_NAME,'td')][8])
 
     
-            for i in range(1,len(rows)):
+            for i in range(len(rows)):
                 try:
-                    WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.XPATH,"//div[contains(text(),'Access email')]/ancestor::button")))
-                    rows[i].find_element(By.XPATH,"//div[contains(text(),'Access email')]/ancestor::button").click()
-                    WebDriverWait(self.driver,15).until(EC.presence_of_all_elements_located((By.XPATH,"//div[contains(@id,'id-')]//span[contains(text(),'@')]")))
-                    email_tags = rows[i].find_elements(By.XPATH,"//div[contains(@id,'id-')]//span[contains(text(),'@')]")
-                    emails = [tag.text for tag in email_tags]
-                
+                    WebDriverWait(self.driver,10).until(EC.element_to_be_clickable((By.XPATH,"//span[contains(text(),'Access email')]/ancestor::button")))
+                    rows[i].find_element(By.XPATH,"//span[contains(text(),'Access email')]/ancestor::button").click()
+                    WebDriverWait(self.driver,60).until(EC.presence_of_element_located((By.XPATH,"//span[contains(text(),'@citadel.com')]")))
+                    #email_tags = rows[i].find_elements(By.XPATH,"//div[contains(@id,'id-')]//span[contains(text(),'@')]")
+                    #emails = [tag.text for tag in email_tags]
+                    #emails = row[i].find_element(By.XPATH,"//span[contains(text(),'@citadel.com')]").get_attribute("textContent")
+                    
                 except:
-                    emails = ['NA']
-                emails_data.append("|".join(emails))
+                    pass
                 self.driver.find_element(By.TAG_NAME,'body').send_keys(Keys.ESCAPE)
-
                 time.sleep(random.randint(2,5))
+            email_tags = self.driver.find_elements(By.XPATH,"//div[contains(@id,'table-row')]/div[4]//span[contains(text(),'@')]")
+            for email_tag in email_tags:
+                full_emails_data.append(email_tag.get_attribute("textContent"))
 
-            phones_text = [item.text for item in self.driver.find_elements(By.XPATH,'//table//tr')][1:]
+            #full_emails_data.append(emails)
+
+            #phones_text = [item.text for item in self.driver.find_elements(By.XPATH,'//table//tr')][1:]
         
-            for item in phones_text:
-                try:
-                    phone_numbers.append(re.search(r"\+?\(?\d{3}?\)?\d{5,10}",item).group())
-                except:
-                    phone_numbers.append('NA')
+            for row in rows:
 
-            full_emails_data.extend(emails_data)
-            full_phone_numbers.extend(phone_numbers)
+                try:
+                    full_phone_numbers.append(row.find_element(By.XPATH,"./div[5]").get_attribute('textContent'))
+
+                except:
+                    full_phone_numbers.append('NA')
+
+            #full_emails_data.extend(emails_data)
+            #full_phone_numbers.extend(phone_numbers)
 
             counter += 1
             if counter == 2:
                 break
-            self.driver.find_element(By.CSS_SELECTOR,"button[aria-label='right-arrow']").click()
-            #WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.XPATH,'//table//tr')))
+            self.driver.find_element(By.CSS_SELECTOR,"button[aria-label='Next']").click()
+            WebDriverWait(self.driver,20).until(EC.presence_of_all_elements_located((By.XPATH,"//div[contains(@id,'table-row')]")))
     
-        full_profile_url = ['|'.join(val) for val in profile_urls]
-        full_company_urls = ['|'.join(item) for item in company_urls]
+        #full_profile_url = ['|'.join(val) for val in profile_urls]
+        #full_company_urls = ['|'.join(item) for item in company_urls]
 
 
         lead_data = pd.DataFrame({'Names':names,'Titles':titles,'Profile_urls':full_profile_url,
              'Comapny_urls':full_company_urls,'Company_names':companies,
-              'Contact_locations':contact_locations,'Employee_counts':employees,
-             'Industries':industries,'Keywords':keywords,'Email_address':full_emails_data,'Phone_Contact':full_phone_numbers})
+              'Email_address':full_emails_data,'Phone_Contact':full_phone_numbers})
         
         
         lead_data.to_csv("apollo_lead.csv",index=False)
@@ -214,3 +246,6 @@ if __name__ == "__main__":
 
     scraper.close_browser()
 
+
+
+#https://app.apollo.io/#/people?sortAscending=false&page=1&organizationIds[]=54a22f23746869331840e813
